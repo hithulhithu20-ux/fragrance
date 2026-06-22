b6705ce8140eddfbbfa2b7cdcd782ff2648cb314
hithulhithu20-ux <hithulhithu20@gmail.com>
2026-06-22 12:46:30 +0530

    first commit

--- project-task/vite.config.js
+++ project-task/vite.config.js
@@ -1,21 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target:
-//           'https://dilkacenter-eqagg4ame5dqdfae.canadacentral-01.azurewebsites.net',
-//         changeOrigin: true,
-//         secure: true,
-//       },
-//     },
-//   },
-// })
-
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
